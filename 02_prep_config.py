ef25c9ed5da1a57c10602991b8064f66d4ea9067
Ting-Yat Wong <yatyat0321@gmail.com>
2021-03-31 02:11:32 +0800

    first commit

---
+++ 02_prep_config.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import click
+import os
+import path
+
+
+def intro(projname, rawdir, outdir, containerdir):
+    """
+    projname = "eyegaze"
+    srcdir = '/home/oldserver/eyegazetask'
+    destdir = '/home/share/eyegaze_BIDS'
+    container = "/home/share/Containers/tjhendrickson-BIDS_scripts-master-latest.simg"
+    """
+    str = []
+    tmp = f'projname = "{projname}"\n'
+    str.append(tmp)
+    tmp = f'rawdir = "{rawdir}"\n'
+    str.append(tmp)
+    tmp = f'outdir = "{outdir}"\n'
+    str.append(tmp)
+    tmp = f'container = "{containerdir}"\n'
+    str.append(tmp)
+    return ''.join(str)
+
+
+@click.command()
+@click.argument("subjid_init", type=int, required=True)
+@click.option("-i", "--input", type=click.Path(exists=True),
+              help="dicomlist.txt", required=True)
+@click.option("--projname", prompt="Project Name", required=True)
+@click.option("--rawdir", prompt="Raw Directory", required=True)
+@click.option("--outdir", prompt="Output Directory", required=True)
+@click.option("--containerdir", prompt="Container Directory", required=True)
+def main(subjid_init, input, projname, rawdir, outdir, containerdir):
+    """
+    Prepare a config.py from a dicomlist.py generated in the source directory
+    (e.g. /home/oldserver/eyegazetask) with the following command:
+    $ find /home/oldserver/eyegazetask -name "DICOM" > dicomlist.txt
+    """
+    # read in dicom directory paths from stdin
+    dicomdirs = []
+    f = open(input)
+    for line in f.readlines():
+        dicomdirs.append(line.rstrip())
+    f.close()
+
+    dicomdirs = sorted(dicomdirs)
+
+    fo = open("config.py", "w")
+    fo.write(intro(projname, rawdir, outdir, containerdir))
+    fo.write("datasets = [\n")
+    for dir_ in dicomdirs:
+        session_init = 0
+        fo.write(f'    ["{dir_}", "{subjid_init:03d}", "{session_init:04d}"],\n')
+        subjid_init += 1
+        session_init += 1
+    fo.write("]")
+    fo.close()
+
+    print("\nMESSAGE: A config.py file is written!")
+
+
+if __name__ == '__main__':
+    main()
---
+++ 04_run_dcm2bids.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+
+import os
+import click
+import shutil
+from tqdm import tqdm
+from pathlib import Path
+from time import sleep
+
+# Paremeters are read from the config.py file
+import config
+
+
+def _parse_range(str_):
+    """
+    Parse a string like numeric index set to a list
+    INPUT : "0-2,5,9-11"
+    OUTPUT: [0, 1, 2, 5, 9, 10, 11]
+    """
+    list_ = set()
+    for chunk in str_.split(','):
+        unit = chunk.split('-')
+        list_.update(range(int(unit[0]), int(unit[-1]) + 1))
+    return sorted(list_)
+
+
+def _copy_allfiles(source, destination):
+    """"Copy all files in a directory to a destination"""
+    for file in os.listdir(source):
+        shutil.copy2(file, destination)
+
+
+@click.command()
+@click.argument("range")
+@click.argument("dcmconfig", type=click.Path(exists=True))
+@click.option("--dryrun", is_flag=True, help="print cmd only")
+@click.option("--forcecopy", is_flag=True, help="force copy of dicom data")
+def main(range, dryrun, dcmconfig, forcecopy):
+    """
+    Converting DICOM images to a BIDS format\n
+    RANGE     : a str of index, e.g. 0-2,5,9-11\n
+    DCMCONFIG: a path of config.json for dicom files
+    """
+    cwd = Path.cwd()
+
+    bidsdir = Path(config.outdir, "BIDS_out")
+    if not bidsdir.is_dir():
+        bidsdir.mkdir(parents=True, exist_ok=True)
+
+    # create if tmp_dcm folder does not exist
+    tmpdcm = Path(config.rawdir, "tmp_dcm")
+    if not tmpdcm.is_dir():
+        tmpdcm.mkdir(parents=True, exist_ok=True)
+
+    subjlist = _parse_range(range)
+
+    if not dryrun:
+        pbar = tqdm(total=len(subjlist), unit="subject", desc="Transforming",
+                    colour="#BDC0BA")
+
+    for subj in subjlist:
+        selected_subj = config.datasets[subj]
+        shortpath = selected_subj[0]
+        subjid = selected_subj[1]
+        session = selected_subj[2]
+
+        subj_tmpdcm = Path(tmpdcm, subjid, session)
+        if forcecopy:
+            # check if subj_tmpdcm exists then delete it
+            if subj_tmpdcm.is_dir():
+                shutil.rmtree(subj_tmpdcm)
+            # create the new destinaton directory
+            subj_tmpdcm.mkdir(parents=True, exist_ok=True)
+            # copy dicom files from raw data to a tmp folder
+            #_copy_allfiles(config.rawdir, subj_tmpdcm)
+        else:
+            if not subj_tmpdcm.is_dir():
+                # create the new destinaton directory
+                subj_tmpdcm.mkdir(parents=True, exist_ok=True)
+                # copy dicom files from raw data to a tmp folder
+                #_copy_allfiles(config.rawdir, subj_tmpdcm)
+            else:
+                print("Copy is not performed!")
+
+        # create the conversion command using dcm2bids
+        # example:
+        #   dcm2bids -d DICOM_DIR -p PARTICIPANT_ID -s SESSION_ID \
+        #   -c CONFIG_FILE -o BIDS_DIR
+        logdir = Path(config.outdir, "log")
+        if not logdir.is_dir():
+            logdir.mkdir(parents=True, exist_ok=True)
+
+        cmd = f"dcm2bids -d {subj_tmpdcm} -p {subjid} -s {session} \
+                -c {dcmconfig} -o {bidsdir}"
+
+        if dryrun:
+            print(cmd)
+        else:
+            # save log
+            cmd = f"{cmd} > {logdir}/{subjid}-{session}.log"
+            pbar.update(1)
+            os.system(cmd)
+    if not dryrun:
+        pbar.close()
+
+
+if __name__ == '__main__':
+    main()
