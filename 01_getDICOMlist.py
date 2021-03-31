3ba12ad9c97d617b9abf5a3ddf370a03f2421a8f
Ting-Yat Wong <yatyat0321@gmail.com>
2021-03-31 16:38:43 +0800

    update step 01

--- 01_getDICOMlist.py
+++ 01_getDICOMlist.py
@@ -3,9 +3,11 @@ import click
 
 
 @click.command()
-@click.option("-p", "--path", type=click.Path(exists=True), required=True)
+@click.option("-p", "--path", type=click.Path(exists=True), required=True,
+              help="a path to a project folder containing raw dicom files")
 def main(path):
-    cmd = f"find {path} -name 'DICOM' > dicomlist.txt"
+    """find all the dicom sub-diretories under a project folder"""
+    cmd = f"find {path} -name DICOM > dicomlist.txt"
     print(cmd)
     os.system(cmd)
 
