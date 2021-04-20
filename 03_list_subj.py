d8f45cf9ede13b060bca8708af8fa00e9914a7ec
Ting-Yat Wong <yatyat0321@gmail.com>
2021-04-20 21:44:18 +0800

    updated step 3

--- 03_list_subj.py
+++ 03_list_subj.py
@@ -3,21 +3,18 @@ import click
 import config
 
 
-@click.command()
-@click.option("--listing", required=True, is_flag=True, help="list the sessions with index")
-def main(listing):
-    if listing:
-        dash = "-" * 80
-        print(dash)
-        print(f"{'Index' : <6}{'Path':^60}{'SubjID':^7}{'Session':^7}")
-        print(dash)
-        count = 0
-        for subjinfo in config.datasets:
-            shortpath = subjinfo[0]
-            subjid = subjinfo[1]
-            session = subjinfo[2]
-            print(f"{count : <6}{shortpath:<60}{subjid:^7}{session:^7}")
-            count += 1
+def main():
+    dash = "-" * 80
+    print(dash)
+    print(f"{'Index' : <6}{'DICOM Directories':^60}{'SubjID':^7}{'Session':^7}")
+    print(dash)
+    count = 0
+    for subjinfo in config.datasets:
+        shortpath = subjinfo[0]
+        subjid = subjinfo[1]
+        session = subjinfo[2]
+        print(f"{count : <6}{shortpath:<60}{subjid:^7}{session:^7}")
+        count += 1
 
 
 if __name__ == '__main__':
