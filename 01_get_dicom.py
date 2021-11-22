e432895f3cfc06efd115321fccac1e891039b62e
Ting-Yat Wong <yatyat0321@gmail.com>
2021-11-22 16:54:09 +0800

    [add] regex argument

--- 01_get_dicom.py
+++ 01_get_dicom.py
@@ -6,11 +6,13 @@ import click
 # Main -------------------------------------------------------------------------
 
 @click.command()
-@click.option("-p", "--path", type=click.Path(exists=True), required=True,
-              help="a path to a project folder containing raw dicom files")
-def main(path):
-    """find all the dicom sub-diretories under a project folder"""
-    cmd = f"find {path} -name DICOM > dicomlist.txt"
+@click.option('-p', '--path', type=click.Path(exists=True), required=True,
+              help='a path to a project folder containing raw dicom files')
+@click.option('-k', '--keyword', default='DICOM', required=True,
+              help='a path to a project folder containing raw dicom files')
+def main(path, keyword):
+    '''find all the dicom sub-diretories under a project folder'''
+    cmd = f'find {path} -name {keyword} > dicomlist.txt'
     print(cmd)
     os.system(cmd)
 
