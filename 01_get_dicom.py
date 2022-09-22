507cd9da7dd5be72b145f466e9dc83755f5a30d1
Ting-Yat Wong <yatyat0321@gmail.com>
2022-09-21 21:39:37 -0400

    [feat] add keyword 2

--- 01_get_dicom.py
+++ 01_get_dicom.py
@@ -8,11 +8,13 @@ import click
 @click.command()
 @click.option('-p', '--path', type=click.Path(exists=True), required=True,
               help='a path to a project folder containing raw dicom files')
-@click.option('-k', '--keyword', default='DICOM', required=True,
+@click.option('-k1', '--keyword1', default='DICOM', required=True,
               help='a path to a project folder containing raw dicom files')
-def main(path, keyword):
+@click.option('-k2', '--keyword2', default='A', required=True,
+              help='a path to a project folder containing raw dicom files')
+def main(path, keyword1, keyword2):
     '''find all the dicom sub-diretories under a project folder'''
-    cmd = f'find {path} -name {keyword} > dicomlist.txt'
+    cmd = f'find {path} \( -name {keyword1} -o -name {keyword2} \) > dicomlist.txt'
     print(cmd)
     os.system(cmd)
 
